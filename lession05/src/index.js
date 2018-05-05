fb1d17a7b3ba80580e6fd047c1904553f20fa409
Tri Nguyen <zymethyang@gmail.com>
2018-05-05 10:01:15 +0700

    add react to webpack

---
+++ lession05/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../src/components/App/App';
+import { createStore, applyMiddleware } from 'redux';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
---
+++ lession05/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                test: /\.css$/
+            },
+            {
+                loader: 'file-loader',
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$|\.ico$/
+            }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery',
+            'window.$': 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ]
+}
